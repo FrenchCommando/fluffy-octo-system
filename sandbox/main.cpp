6076ad600c2b8a00d473ed538e9c88315ed68015
FrenchCommando <martialren@gmail.com>
2019-06-07 14:13:26 -0400

    sample_file added to sandbox

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -2,15 +2,25 @@
 #include <input_parser.h>
 #include <numeric_helper.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-
+void run_input_parser(){
     input_parser::get_int(1);
     input_parser::use_scanf();
     input_parser::initials();
     std::cout << input_parser::digit(8) << std::endl;
+    input_parser::parse_int("123h3213,12321ttt.3213");
+}
+
+void run_numeric_helper(){
     std::cout << numeric_helper::vmax(12., 13., 20., 10.) <<std::endl;
     std::cout << numeric_helper::amax(12., 13., 20., 10.) <<std::endl;
-    input_parser::parse_int("123h3213,12321ttt.3213");
+}
+
+void run_one();
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+//    run_input_parser();
+//    run_numeric_helper();
+    run_one();
     return 0;
 }
---
+++ sandbox/sample_file.cpp
@@ -0,0 +1,9 @@
+//
+// Created by frenchcommando on 6/7/2019.
+//
+
+#include <iostream>
+
+void run_one(){
+    std::cout << "I'm here" << std::endl;
+}
